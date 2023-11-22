87a4cd95777184486bc16d5136296e948bd6a7f6
Lim Mikhail <129537885+2023git@users.noreply.github.com>
2023-11-22 19:00:42 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import sortBy from 'sort-by';
 import {CSSTransitionGroup} from 'react-transition-group';
 import SwipeableViews from 'react-swipeable-views';
 import AppBar from 'material-ui/AppBar';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'; 
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton'; 
 import {Tabs, Tab} from 'material-ui/Tabs';
