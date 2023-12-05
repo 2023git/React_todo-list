e640f0cb1d1ddffad82b957cea81acdb65bb9f68
Lim Mikhail <129537885+2023git@users.noreply.github.com>
2023-12-05 23:55:22 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import EditIcon from 'material-ui/svg-icons/action/delete';
 import CloseIcon from 'material-ui/svg-icons/content/delete-sweep'; 
 import ColumnList from './ColumnList';      
 import ConfirmDialog from './ConfirmDialog';    
-import If from './If';
+import If from './If';   
 import './App.css';  
       
 /**
